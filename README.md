5e3ab8338364649efe9af8fb6fee5f80333ab88a
LunaMora425 <lunamora425@gmail.com>
2026-02-26 15:27:25 -0500

    initial plugin set up

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# **Plugin Name** Plugin
+# **Barbermonger Custom Automation** Plugin
 
 **Plugin Summary**
 
-For more information, please see: **url to meta topic**
+Extending the Discourse Automation plugin for BM's use case. Specifically aiming to add an automatic archival process and the ability to auto-move search topics posted in the wrong place.
+
+<!-- For more information, please see: **url to meta topic** -->
--- plugin.rb
+++ plugin.rb
@@ -1,21 +1,40 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
-# about: TODO
-# meta_topic_id: TODO
+# name: bm-discourse-automation
+# about: Extending the Discourse Automation plugin for BM's use case
 # version: 0.0.1
-# authors: Discourse
-# url: TODO
+# authors: LunaMora
+# url: https://github.com/LunaMora425/bm-discourse-automation
 # required_version: 2.7.0
 
-enabled_site_setting :plugin_name_enabled
-
-module ::MyPluginModule
-  PLUGIN_NAME = "discourse-plugin-name"
-end
-
-require_relative "lib/my_plugin_module/engine"
+enabled_site_setting :bm_discourse_automation_enabled
 
 after_initialize do
-  # Code which should run after Rails has finished booting
+
+  # ============================================================
+  # AUTOMATION 1: Archive Mover
+  # Moves topics to a paired archive category when archived.
+  # Config: JSON map of { "source-slug" => "archive-slug" }
+  # ============================================================
+
+  on(:topic_status_updated) do |topic, status, enabled|
+    next unless status == "archived" && enabled
+
+    source_category = topic.category
+    next unless source_category
+
+    raw_map = SiteSetting.bm_archive_category_map
+    archive_map = JSON.parse(raw_map) rescue {}
+
+    archive_slug = archive_map[source_category.slug]
+    next unless archive_slug
+
+    archive_category = Category.find_by(slug: archive_slug)
+    next unless archive_category
+    next if topic.category_id == archive_category.id
+
+    topic.change_category_to_id(archive_category.id)
+    topic.save!
+  end
+
 end
